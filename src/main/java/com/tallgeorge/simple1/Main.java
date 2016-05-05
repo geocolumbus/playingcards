1eb3b29e3915cc3d3e5cfe29f8a2f62786a11428
George Campbell <campbelg@oclc.org>
2016-05-05 16:29:11 -0400

    Search for royal flushes dealt first time.

--- src/main/java/com/tallgeorge/simple1/Main.java
+++ src/main/java/com/tallgeorge/simple1/Main.java
@@ -2,15 +2,27 @@ package com.tallgeorge.simple1;
 
 import com.tallgeorge.simple1.deck.CardDeck;
 import com.tallgeorge.simple1.deck.Deck;
+import com.tallgeorge.simple1.deck.Hand;
+import com.tallgeorge.simple1.deck.PlayingHand;
+import com.tallgeorge.simple1.deck.PokerHandEnum;
 
 /**
  * Created by campbelg on 5/3/16.
  */
 public class Main {
     public static void main(String[] args) {
-        Deck deck = new CardDeck();
-        System.out.println(deck);
-        deck.shuffle();
-        System.out.println(deck);
+        for (int i=0; i<2000000; i++) {
+            Deck deck = new CardDeck();
+            deck.shuffle();
+            Hand hand = new PlayingHand();
+            hand.add(deck.pullFromTop());
+            hand.add(deck.pullFromTop());
+            hand.add(deck.pullFromTop());
+            hand.add(deck.pullFromTop());
+            hand.add(deck.pullFromTop());
+            if (PokerHandEnum.find(hand) == PokerHandEnum.ROYAL_FLUSH) {
+                System.out.println(i + " " + hand);
+            }
+        }
     }
 }
