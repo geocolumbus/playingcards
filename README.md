aba92895205575b4937512f6eea7f74bc2c93b7f
George Campbell <campbelg@oclc.org>
2017-11-02 11:12:43 -0400

    Update readme, usage instructions. Make jar runnable.

--- README.md
+++ README.md
@@ -2,19 +2,43 @@
 
 Building out a card game framework. Working it from a strict TDD approach as an exercise.
 
-## Odds of being dealt a given hand from a shuffled deck
+## Requirements
 
-<pre>              
-     ROYAL_FLUSH   0.000155 (1 in  644890.0)
-  STRAIGHT_FLUSH   0.001385 (1 in   72225.6)
-  FOUR_OF_A_KIND   0.024031 (1 in    4161.4)
-      FULL_HOUSE   0.144061 (1 in     694.1)
-           FLUSH   0.196430 (1 in     509.1)
-        STRAIGHT   0.353143 (1 in     283.2)
- THREE_OF_A_KIND   2.108099 (1 in      47.4)
-        TWO_PAIR   4.730096 (1 in      21.1)
-        ONE_PAIR  40.158516 (1 in       2.5)
-       HIGH_CARD  47.118204 (1 in       2.1)
+This is what I'm running - lesser minor versions will probably work fine.
 
-Duration was 4878.025 seconds for 2,147,483,647 iterations at 2.272 μsec/iteration.
-</pre>
+* maven 3.3.9
+* java 1.8.0_101
+
+## Usage
+
+```
+git clone https://github.com/geocolumbus/playingcards.git
+mvn clean package
+java -jar target/playingcards-1.0-SNAPSHOT.jar
+```
+
+## Results
+
+The odds of being dealt a given five card hand from a shuffled deck:
+
+```
+Creating and evaluating 134,217,727 poker hands...
+
+Duration was 262.610 seconds for 134,217,727 iterations at 1.957 usec/iteration.
+
+     ROYAL_FLUSH   0.000160 (1 in  624268.5)
+  STRAIGHT_FLUSH   0.001353 (1 in   73908.4)
+  FOUR_OF_A_KIND   0.024022 (1 in    4162.8)
+      FULL_HOUSE   0.143760 (1 in     695.6)
+           FLUSH   0.196405 (1 in     509.2)
+        STRAIGHT   0.353426 (1 in     282.9)
+ THREE_OF_A_KIND   2.109090 (1 in      47.4)
+        TWO_PAIR   4.733815 (1 in      21.1)
+        ONE_PAIR  40.082394 (1 in       2.5)
+       HIGH_CARD  46.948342 (1 in       2.1)
+         GARBAGE   0.000000 (1 in  Infinity)
+```
+
+Compare this result with the official odds of being dealt a hand. The more hands tried, the more accurate the results.
+
+[Wikipedia: Frequency of 5-card poker hands](https://en.wikipedia.org/wiki/Poker_probability#Frequency_of_5-card_poker_hands)
--- src/main/java/com/tallgeorge/playingcards/Main.java
+++ src/main/java/com/tallgeorge/playingcards/Main.java
@@ -19,6 +19,7 @@ public class Main {
 
     /**
      * Main entrance to the application.
+     *
      * @param args command line argument array.
      */
     public static void main(final String[] args) {
@@ -26,9 +27,11 @@ public class Main {
         for (PokerHandEnum p : PokerHandEnum.values()) {
             bin.put(p, 0);
         }
-        int total = Integer.MAX_VALUE / 64;
+        int numberOfPokerHands = Integer.MAX_VALUE / 16;
+        DecimalFormat formatter = new DecimalFormat("#,###");
+        System.out.println(String.format("\nCreating and evaluating %s poker hands...",formatter.format(numberOfPokerHands)));
         Instant start = Instant.now();
-        IntStream.range(0, total).parallel().forEach(i -> {
+        IntStream.range(0, numberOfPokerHands).parallel().forEach(i -> {
             Deck deck = new CardDeck();
             deck.shuffle();
             Hand hand = deck.deal(5);
@@ -36,15 +39,16 @@ public class Main {
             bin.put(p, bin.get(p) + 1);
         });
         Instant stop = Instant.now();
-        System.out.println("");
+
+        Double sec = Duration.between(start, stop).toMillis() / 1000.0;
+        System.out.println(String.format("\nDuration was %6.3f seconds for %s iterations at %5.3f usec/iteration.\n",
+                sec, formatter.format(numberOfPokerHands), sec / numberOfPokerHands * 1e6));
+
         bin.keySet().stream().forEach(k -> {
-            double p = (bin.get(k) * 1.0) / total * 100.0;
+            double p = (bin.get(k) * 1.0) / numberOfPokerHands * 100.0;
             double o = (100 / p);
             System.out.println(String.format("%16s  %9.6f (1 in %9.1f)", k, p, o));
         });
-        Double sec = Duration.between(start, stop).toMillis() / 1000.0;
-        DecimalFormat formatter = new DecimalFormat("#,###");
-        System.out.println(String.format("\nDuration was %6.3f seconds for %s iterations at %5.3f usec/iteration.",
-            sec, formatter.format(total), sec / total * 1e6));
+
     }
 }
