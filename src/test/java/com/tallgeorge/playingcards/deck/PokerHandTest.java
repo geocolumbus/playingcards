fefebb00847714542e45e77ddc8aebdf65d335ae
George Campbell <geocolumbus@gmail.com>
2019-03-05 20:41:54 -0500

    Correct comments in test.

--- src/test/java/com/tallgeorge/playingcards/deck/PokerHandTest.java
+++ src/test/java/com/tallgeorge/playingcards/deck/PokerHandTest.java
@@ -19,7 +19,7 @@ public class PokerHandTest {
         hand.add(PlayingCardEnum.KING_HEART);
         hand.add(PlayingCardEnum.ACE_HEART);
         assertEquals("Should detect a Royal Flush.", ROYAL_FLUSH, PokerHandEnum.find(hand));
-        assertEquals("Should be score of two.", 1, PokerHandEnum.find(hand).getScore());
+        assertEquals("Should be score of one.", 1, PokerHandEnum.find(hand).getScore());
     }
 
     @Test
@@ -43,7 +43,7 @@ public class PokerHandTest {
         hand.add(PlayingCardEnum.THREE_DIAMOND);
         hand.add(PlayingCardEnum.SIX_SPADE);
         assertEquals("Should detect Four of a Kind, high extra card.", FOUR_OF_A_KIND, PokerHandEnum.find(hand));
-        assertEquals("Should be score of six.", 5, PokerHandEnum.find(hand).getScore());
+        assertEquals("Should be score of five.", 5, PokerHandEnum.find(hand).getScore());
 
         hand.add(PlayingCardEnum.THREE_SPADE);
         hand.add(PlayingCardEnum.THREE_HEART);
@@ -63,7 +63,7 @@ public class PokerHandTest {
         hand.add(PlayingCardEnum.FOUR_DIAMOND);
         hand.add(PlayingCardEnum.THREE_DIAMOND);
         assertEquals("Should detect full house, high pair.", FULL_HOUSE, PokerHandEnum.find(hand));
-        assertEquals("Should be score of six.", 3, PokerHandEnum.find(hand).getScore());
+        assertEquals("Should be score of three.", 3, PokerHandEnum.find(hand).getScore());
 
         hand.add(PlayingCardEnum.THREE_SPADE);
         hand.add(PlayingCardEnum.TWO_HEART);
@@ -71,7 +71,7 @@ public class PokerHandTest {
         hand.add(PlayingCardEnum.TWO_DIAMOND);
         hand.add(PlayingCardEnum.THREE_DIAMOND);
         assertEquals("Should detect full house, low pair.", FULL_HOUSE, PokerHandEnum.find(hand));
-        assertEquals("Should be score of one.", 2, PokerHandEnum.find(hand).getScore());
+        assertEquals("Should be score of two.", 2, PokerHandEnum.find(hand).getScore());
     }
 
     @Test
